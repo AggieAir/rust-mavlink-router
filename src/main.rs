04ba40c4f3c92ba372ccb9988e0dab1f5a1c7a15
Richard Snider <lighthousemaniac@gmail.com>
2023-08-07 14:12:50 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,133 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+use std::collections::HashMap;
+use std::time::Duration;
+
+// use tokio::task;
+
+use mavlink::{self, common, Message};
+// use futures::{prelude::*, self};
+//
+// struct MessageStream<M: mavlink::Message, MC: mavlink::MavConnection<M>> {
+//     connection: MC,
+//     future: Option<Future<Output = M>>,
+//     phantom: PhantomData<M>,
+// }
+//
+// impl<M: mavlink::Message, MC: mavlink::MavConnection<M>> MessageStream<M, MC> {
+//     fn new(connection: MC) -> MessageStream<M, MC> {
+//         MessageStream { connection }
+//     }
+// }
+//
+// impl<M: mavlink::Message, MC: mavlink::MavConnection<M>> Stream for MessageStream<M, MC> {
+//     type Item = M;
+//
+//     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+//     }
+// }
+
+// #[tokio::main(flavor = "current_thread")]
+fn main() {
+    // let urls = vec!["udpin:192.168.42.142:14550", "udpbcast:192.168.42.255:14560"];
+    // let urls = vec!["udpin:0.0.0.0:14560", "udpout:127.0.0.1:14570"];
+    // let urls = vec!["udpout:127.0.0.1:14570"];
+    
+    let urls = std::env::args().skip(1).collect::<Vec<_>>();
+
+    if urls.len() == 0 {
+        eprintln!("No arguments, exiting now");
+        std::process::exit(1);
+    }
+
+    let mut raw_connections = Vec::with_capacity(urls.len());
+
+    let map: Arc<Mutex<HashMap<(u8, u8), u8>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    for (idx, url) in urls.iter().enumerate() {
+        println!("opening connection to {} (#{})", url, idx);
+        let mut connection = mavlink::connect::<common::MavMessage>(url).unwrap();
+        connection.set_protocol_version(mavlink::MavlinkVersion::V2);
+        raw_connections.push((idx, Arc::new(connection)));
+    }
+
+    let connections = Arc::new(raw_connections);
+
+    let join_handles = connections
+        .iter()
+        .map(|(idx, connection)| {
+            let my_connections = Arc::clone(&connections);
+            let my_connection = Arc::clone(&connection);
+            let my_idx = idx.clone();
+            let my_map = map.clone();
+
+            thread::spawn(move || {
+                println!("starting thread {}", my_idx);
+                loop {
+                    // println!(
+                    //     "endpoint #{}: awaiting message",
+                    //     my_idx,
+                    // );
+
+                    let (header, message) = my_connection.recv().unwrap();
+
+                    let id = (header.system_id, header.component_id);
+
+                    println!(
+                        "endpoint #{}: message 0x{:02X} from ({:3}, {:3}) -- id={:3} {:?}",
+                        my_idx,
+                        header.sequence, header.system_id, header.component_id,
+                        message.message_id(), message.message_name()
+                    );
+
+                    for (recv_idx, receiver) in my_connections.as_slice() {
+                        if *recv_idx != my_idx {
+                            receiver.send(&header, &message).unwrap();
+                        }
+                    }
+
+                    my_map.lock().unwrap().entry(id)
+                        .and_modify(|seq| {
+                            if seq.overflowing_add(1).0 != header.sequence {
+                                println!("endpoint #{}: missing {} values (prev={}, next={})", my_idx, header.sequence.overflowing_sub(*seq).0, *seq, header.sequence);
+                            }
+                            *seq = header.sequence;
+                        })
+                        .or_insert_with(|| header.sequence);
+                }
+            })
+        })
+        .collect::<Vec<_>>();
+
+    // for i in 0..10 {
+    //     for (idx, conn) in connections.as_slice() {
+    //         println!("sending to #{}", idx);
+    //         conn.send(
+    //             &mavlink::MavHeader {
+    //                 system_id: 254,
+    //                 component_id: 190,
+    //                 sequence: 0,
+    //             },
+    //             // &common::MavMessage::PARAM_REQUEST_LIST(common::PARAM_REQUEST_LIST_DATA {
+    //             //     target_system: 1,
+    //             //     target_component: 0,
+    //             // }),
+    //             &common::MavMessage::HEARTBEAT(common::HEARTBEAT_DATA {
+    //                 custom_mode: 0,
+    //                 mavtype: common::MavType::MAV_TYPE_GENERIC,
+    //                 autopilot: common::MavAutopilot::MAV_AUTOPILOT_INVALID,
+    //                 base_mode: common::MavModeFlag::empty(),
+    //                 system_status: common::MavState::MAV_STATE_STANDBY,
+    //                 mavlink_version: 0,
+    //             }),
+    //         ).unwrap();
+    //     }
+
+    //     std::thread::sleep(std::time::Duration::new(1, 0));
+    // }
+
+    for jh in join_handles {
+        jh.join().unwrap();
+    }
+}
